b60dab591469997c23dfb5f333384f317f673ba6
Sonu Kumar <sonu.kumar@zs.com>
2020-01-24 17:06:10 +0530

    odified package.json

--- server.js
+++ server.js
@@ -1,25 +1,10 @@
-// var express = require("express");
-// var app = express();
-// app.use(express.static(__dirname + '/dist'))
-// app.get('/',function(req , res){
-// 	res.status(200).send('OK')
-// })
-// app.listen(4200,function(request , response){
-	
-// });
-//Install express server
-const express = require('express');
-const path = require('path');
+var express = require("express");
+var app = express();
+app.use(express.static(__dirname + '/dist/MuseUI'))
+app.get('/',function(req , res){
+    res.sendFile(path.join(__dirname+'/dist/MuseUI/index.html'));
+    res.status(200).send('OK')
+})
 
-const app = express();
-
-// Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/MuseUI'));
-
-app.get('/*', function(req,res) {
-    
-res.sendFile(path.join(__dirname+'/dist/MuseUI/index.html'));
-});
-
-// Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
+// Start the app by listening on the default Heroku port
