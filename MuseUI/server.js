a2f6d7cc1c50d7e45c384ca57379fab1aa5a0a56
Sonu Kumar <sonu.kumar@zs.com>
2020-01-24 13:52:10 +0530

    done chnages for hosting on heroku

--- MuseUI/server.js
+++ MuseUI/server.js
@@ -1,9 +1,25 @@
-var express = require("express");
-var app = express();
-app.use(express.static(__dirname + '/dist'))
-app.get('/',function(req , res){
-	res.status(200).send('OK')
-})
-app.listen(4200,function(request , response){
+// var express = require("express");
+// var app = express();
+// app.use(express.static(__dirname + '/dist'))
+// app.get('/',function(req , res){
+// 	res.status(200).send('OK')
+// })
+// app.listen(4200,function(request , response){
 	
+// });
+//Install express server
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+// Serve only the static files form the dist directory
+app.use(express.static(__dirname + '/dist/MuseUI'));
+
+app.get('/*', function(req,res) {
+    
+res.sendFile(path.join(__dirname+'/dist/MuseUI/index.html'));
 });
+
+// Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 8080);
